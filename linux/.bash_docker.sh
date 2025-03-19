e67e5025150c1f6bf0dd0d395da74beb5e4fdac3
Oleksii Abrosymov <amberlex78@gmail.com>
2025-03-19 15:52:50 +0200

    add comments

--- linux/.bash_docker.sh
+++ linux/.bash_docker.sh
@@ -1,5 +1,7 @@
+# Description: Docker related functions and aliases
 if command -v docker >/dev/null 2>&1; then
 
+  # List IP addresses of running containers
   function dnames-fn {
     for ID in `docker ps | awk '{print $1}' | grep -v 'CONTAINER'`
     do
@@ -7,6 +9,7 @@ if command -v docker >/dev/null 2>&1; then
     done
   }
 
+  # List IP addresses of running containers
   function dip-fn {
     echo "IP addresses of all named running containers"
 
@@ -19,42 +22,50 @@ if command -v docker >/dev/null 2>&1; then
     unset OUT
   }
 
+  # Remove dangling images
   function drmid-fn {
     imgs=$(docker images -qf dangling=true)
     [ ! -z "$imgs" ] && docker rmi $imgs || echo "no dangling images."
   }
 
+  # Remove all exited containers
   function drmce-fn {
     docker rm $(docker ps --all -qf status=exited)
   }
 
+  # Remove all containers
   function drmcr-fn {
     docker rm -f $(docker ps -aq)
   }
 
+  ########################################
+  # Aliases
+  ########################################
 
-  alias drmid=drmid-fn
-  alias drmce=drmce-fn
-  alias drmcr=drmcr-fn
+  # Remove all unused containers, networks, images (both dangling and unreferenced), and optionally, volumes.
+  alias dprune="docker system prune -a"  
 
-  alias di="docker images"
-  alias dip=dip-fn
+  alias drmid=drmid-fn  # Remove dangling images
+  alias drmce=drmce-fn  # Remove all exited containers
+  alias drmcr=drmcr-fn  # Remove all containers
 
-  # Regular list
-  alias dps="docker ps"
-  alias dpsa="docker ps -a"
-
-  # Format list
-  alias dpsf="docker ps --format 'table {{.ID}}\\t{{.Image}}\\t{{.Names}}\\t{{.Ports}}'"
-  alias dpsaf="docker ps -a --format 'table {{.ID}}\\t{{.Image}}\\t{{.Names}}\\t{{.Ports}}'"
+  alias di="docker images"  # List images
+  alias dip=dip-fn          # List IP addresses of running containers
   
-  alias drmi="docker rmi"
-  alias drm="docker rm"
+  alias dps="docker ps"      # List running containers
+  alias dpsa="docker ps -a"  # List all containers
 
-  alias dnls="docker network ls"
-  alias dnrm="docker network rm"
   
-  alias dvls="docker volume ls"
-  alias dvrm="docker volume rm"
+  alias dpsf="docker ps --format 'table {{.ID}}\\t{{.Image}}\\t{{.Names}}\\t{{.Ports}}'"      # Format list
+  alias dpsaf="docker ps -a --format 'table {{.ID}}\\t{{.Image}}\\t{{.Names}}\\t{{.Ports}}'"  # Format list all
+  
+  alias drmi="docker rmi"  # Remove image
+  alias drm="docker rm"    # Remove container
+
+  alias dnls="docker network ls"  # List networks
+  alias dnrm="docker network rm"  # Remove network
+  
+  alias dvls="docker volume ls"  # List volumes
+  alias dvrm="docker volume rm"  # Remove volume
 
 fi
